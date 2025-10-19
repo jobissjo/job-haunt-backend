2784a948e26b891601743d5c3c3094030e3a0890
Jobi S S <jobisjobi1234@gmail.com>
2025-10-19 17:04:43 +0530

    removed turtle

--- apis/models/user_management.py
+++ apis/models/user_management.py
@@ -1,10 +1,7 @@
-from turtle import mode
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
-    Group,
-    Permission,
 )
 from django.db import models
 from django.core.validators import RegexValidator
